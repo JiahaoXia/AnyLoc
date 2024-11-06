4a19f7280b7f9caf735e277fecf88808f13fd23d
JiahaoXia <jiahaoxia1993@gmail.com>
2024-11-06 10:36:54 -0500

    update gsv dataset

--- custom_datasets/gsv.py
+++ custom_datasets/gsv.py
@@ -58,6 +58,7 @@ class GSV_Dataset(Dataset):
 if __name__ == "__main__":
     base_transform = transforms.Compose(
         [
+            transforms.Resize((400, 800)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
